a17ca75eb1dba02201afe5fcd42e493720aaf9d0
Nguyễn Quốc Cường <60700939+nqcuong20@users.noreply.github.com>
2020-10-11 16:04:47 +0700

    final

--- Scripts/TC02/Script1602392545879.groovy
+++ Scripts/TC02/Script1602392545879.groovy
@@ -16,7 +16,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.openBrowser(GlobalVariable.URL)
 
 WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
 
